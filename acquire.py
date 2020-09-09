932a864110a0638014b246555455e6b8cc7cc123
Corey Solitaire <corey.solitaire@gmail.com>
2020-09-09 08:31:54 -0500

    Function Formatting

--- acquire.py
+++ acquire.py
@@ -1,29 +1,18 @@
 def get_titanic_data():
     filename = "titanic.csv"
-
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
-        # read the SQL query into a dataframe
         df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
-
-        # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_csv(filename, index = False)
+    return df
 
-        # Return the dataframe to the calling code
-        return df
 
 def get_iris_data():
     filename = "iris.csv"
-
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
-        # read the SQL query into a dataframe
         df = pd.read_sql('SELECT * FROM measurements AS m JOIN species AS s on m.species_id = s.species_id', get_connection('iris_db'))
-
-        # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_csv(filename, index = False)
-
-        # Return the dataframe to the calling code
-        return df
+    return df
